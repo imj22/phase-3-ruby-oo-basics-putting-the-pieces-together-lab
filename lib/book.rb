4172a14c525e60545d2abe82cf78f5de940bd06a
imj22 <98693440+imj22@users.noreply.github.com>
2022-05-18 11:49:58 +0200

    commit tree 6477926d914e8631c1b7d84863b893615cb62e69

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+require "pry"
 
+class Book 
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def initialize (title, author = "Agatha Christie", page_count =272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+puts book.title
+puts book.author
+puts book.page_count
+puts book.genre
+book.turn_page
+
+# binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
+require "pry"
+
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize (brand, color = "red", size = 9.5, material = "suede", condition = "tattered")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+adidas = Shoe.new("Adidas")
+
+shoe = Shoe.new("Nike")
+shoe.brand
+shoe.color
+shoe.size 
+shoe.material
+shoe.condition
+# binding.pry
+shoe.cobble
+# binding.pry
+
